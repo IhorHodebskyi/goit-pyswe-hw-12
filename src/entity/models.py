400ee867c9d9007372392b5f27d2c3dd726b18b7
ihor <i0962112822@gmail.com>
2025-05-05 19:43:11 +0300

    init

---
+++ src/entity/models.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Date, Text, Integer, ForeignKey, DateTime, func
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    surname: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    birthday: Mapped[date] = mapped_column(Date, nullable=True)
+    additional_data: Mapped[str] = mapped_column(Text, nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now(), nullable=True)
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now(),
+                                             nullable=True)
+    user_id: Mapped[int] = mapped_column(Integer,
+                                         ForeignKey("users.id"), nullable=True)
+    user: Mapped["User"] = relationship("User", backref="contacts", lazy="joined")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(150), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(150), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
